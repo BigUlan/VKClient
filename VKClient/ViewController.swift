37aeb5d30b1f1622700e4e6bd853176be28ddf10
Улан Бейшенкулов <ulan.beishenkulov@icloud.com>
2023-02-18 21:56:12 +0600

    DEV-001 "First step pass"

--- VKClient/ViewController.swift
+++ VKClient/ViewController.swift
@@ -11,15 +11,18 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var loginTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
-    @IBAction func loginButton(_ sender: UIButton) {
-        
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func loginButton(_ sender: Any) {
+        if loginTextField.text == "w" && passwordTextField.text == "q" {
+            performSegue(withIdentifier: "Login", sender: nil)
+        } else {
+            showAlert(message: "Неверный логин или пароль")
+            return }
+    }
 }
 
