3449adc36b955e8a95f246c59c7010ca1f9a238b
Улан Бейшенкулов <ulan.beishenkulov@icloud.com>
2023-02-18 18:46:39 +0600

    Second commit

---
+++ VKClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VKClient
+//
+//  Created by Ulan Beishenkulov on 18/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBAction func loginButton(_ sender: UIButton) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
