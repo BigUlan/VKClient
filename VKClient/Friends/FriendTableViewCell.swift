e3706bffd2b80568be3dbdaef8178c096a910f60
Улан Бейшенкулов <ulan.beishenkulov@icloud.com>
2023-02-19 22:34:40 +0600

    DEV-002: Second commit - finished third homeWork

---
+++ VKClient/Friends/FriendTableViewCell.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class FriendTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var avatar: UIImageView!
+    @IBOutlet weak var friendName: UILabel!
+    
+}
---
+++ VKClient/Groups/GroupsTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  GroupsTableViewCell.swift
+//  VKClient
+//
+//  Created by Ulan Beishenkulov on 19/2/23.
+//
+
+import UIKit
+
+class GroupsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var icon: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    
+
+}
